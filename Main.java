c7b8d09e0b5a5205dcf9e6eda129fa57c20815ca
mohamedfisal1234 <84065405+mohamedfisal1234@users.noreply.github.com>
2021-05-23 20:44:39 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+	public static void main(String[] args) {
+	 System.out.print("name   ");
+	    System.out.print("year of joining    ");
+	System.out.print("adress");
+			System.out.println()   ;
+
+ System.out.print("robert     ");
+	    System.out.print("1994         ");
+	System.out.print("64c-wallsStreet");
+			System.out.println()   ;
+
+	System.out.print("sam        ");
+	    System.out.print("2000         ");
+	System.out.print("68d-WallsStreet");
+			System.out.println()   ;
+		System.out.print("john       ");
+	    System.out.print("1999         ");
+	System.out.print("26B-WallsStreet");
+			System.out.println()   ;	
+	}
+}
